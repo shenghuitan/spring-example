b1061ffbc64d8a6c4890068ad57a0c4024f678d3
tanshenghui1 <tanshenghui1@yy.com>
2020-09-16 01:19:21 +0800

    disruptor测试：ProducerType.SINGLE：1亿次，8678毫秒，生产端4线程，丢数据量约1%。

--- src/main/java/com/romtn/springexample/disruptor/ExampleEventSingleTest.java
+++ src/main/java/com/romtn/springexample/disruptor/ExampleEventSingleTest.java
@@ -1,48 +1,103 @@
 package com.romtn.springexample.disruptor;
 
+import com.lmax.disruptor.BlockingWaitStrategy;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
 import com.lmax.disruptor.util.DaemonThreadFactory;
+import com.romtn.springexample.util.Counter;
+import com.romtn.springexample.util.Timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ExampleEventSingleTest {
 
     static Logger logger = LoggerFactory.getLogger(ExampleEventSingleTest.class);
 
     RingBuffer<ExampleEvent> ringBuffer;
-    ExampleEventHandler[] handlers;
     ExampleEventTranslator translator;
 
     final int BUFFER_SIZE = 1024;
 
-//    @Before
+    ThreadPoolExecutor producerExecutor;
+
+    Counter pool = new Counter();
+    Counter counter = ExampleEventFactory.counter;
+
+    Timer timer = Timer.init();
+
+
     public void init() {
-        handlers = new ExampleEventHandler[2];
-        handlers[0] = new ExampleEventHandler();
-        handlers[1] = new ExampleEventHandler();
+        Disruptor<ExampleEvent> disruptor = new Disruptor<>(
+                new ExampleEventFactory(), BUFFER_SIZE, DaemonThreadFactory.INSTANCE,
+                ProducerType.SINGLE, new BlockingWaitStrategy());
 
-        Disruptor<ExampleEvent> disruptor = new Disruptor<>(new ExampleEventFactory(), BUFFER_SIZE, DaemonThreadFactory.INSTANCE);
-
-        disruptor.handleEventsWith(handlers[0]);
+        disruptor.handleEventsWith(new ExampleEventHandler());
         disruptor.start();
 
         ringBuffer = disruptor.getRingBuffer();
 
         translator = new ExampleEventTranslator();
+
+        producerExecutor = new ThreadPoolExecutor(POOL_SIZE, POOL_SIZE,
+                KEEP_ALIVE_TIME, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(),
+                r -> {
+                    Thread thread = new Thread(r);
+                    thread.setName("producer-pool-" + pool.received.getAndIncrement());
+                    return thread;
+                });
+
+        counter.limit.set(limit);
+        timer.mark();
     }
 
-//    @Test
-    public void test() throws InterruptedException {
-        for (int i = 0; ; i++) {
+    public void produce() {
+        for (int i = 0; i < producerExecutor.getMaximumPoolSize(); i++) {
+            producerExecutor.execute(() -> produce0());
+        }
+    }
+
+    public void produce0() {
+        for (int i = 0; counter.received.get() < counter.limit.get(); i++) {
+            counter.received.getAndIncrement();
+
             ExampleEvent e = new ExampleEvent();
             e.setId(i);
 
             ringBuffer.publishEvent(translator, e);
-            logger.info("publishEvent event:{}", e);
-
-            Thread.sleep(1000);
+            if (logger.isDebugEnabled()) {
+                logger.debug("publishEvent event:{}", e);
+            }
         }
+        timer.mark();
+    }
+
+    public void monitor() {
+        new Thread(() -> {
+            for (; ; ) {
+                if (counter.received.get() < counter.limit.get()) {
+                    timer.mark();
+                }
+
+                logger.info("counter:{}, timer:{}, costs:{}", counter, timer, timer.costs());
+
+                if (counter.sent.get() >= counter.limit.get()) {
+                    break;
+                }
+
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    logger.error("", e);
+                }
+            }
+
+            System.exit(0);
+        }).start();
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -50,8 +105,8 @@ public class ExampleEventSingleTest {
 
         ExampleEventSingleTest instance = new ExampleEventSingleTest();
         instance.init();
-        instance.test();
-
+        instance.produce();
+        instance.monitor();
     }
 
 }
